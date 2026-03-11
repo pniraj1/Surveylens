beb5cd71071db83f79525f629fbb26de52bd205d
Niraj Vikram Patil <89404809+pniraj1@users.noreply.github.com>
2026-03-11 13:55:13 +0100

    Delete surveylens directory

--- surveylens/src/constants/AdConfig.js
+++
@@ -1,9 +0,0 @@
-// AdMob Ad Unit IDs - Your live keys
-export const AD_UNITS = {
-  BANNER: 'ca-app-pub-6365268430069678/9574699422',
-  INTERSTITIAL: 'ca-app-pub-6365268430069678/8814987800',
-  REWARDED: 'ca-app-pub-6365268430069678/6002644716',
-};
-
-// Show interstitial every N photos
-export const INTERSTITIAL_PHOTO_INTERVAL = 10;
